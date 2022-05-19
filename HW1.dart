399b9e5988d50f504bbdcf931f4595c5957200cf
sarrabdullah <68784644+sarrabdullah@users.noreply.github.com>
2022-05-19 17:45:39 +0300

    Create HW1.dart

---
+++ HW1.dart
@@ -0,0 +1,8 @@
+var Name = "Sarah Abdullah"; // the name of person.
+
+  var DOB = "2/17/2000"; // date of birth.
+
+  const phone = "0508463530"; // phone number.
+
+  print("This is ${Name}\nDate of birth ${DOB}\nphone ${phone}");
+  print("the fourth digit of my number is ${phone[3]} \nI have ${Name.length} letter in my name");
