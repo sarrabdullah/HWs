b2cc08c1895fc04c959aaad74aaa64c8758b3c1d
Sarah <68784644+sarrabdullah@users.noreply.github.com>
2022-06-11 17:20:36 +0300

    Update HW1.dart

--- HW1.dart
+++ HW1.dart
@@ -2,7 +2,7 @@ var Name = "Sarah Abdullah"; // the name of person.
 
   var DOB = "2/17/2000"; // date of birth.
 
-  const phone = "0508463530"; // phone number.
+  const phone = "0000000000"; // phone number.
 
   print("This is ${Name}\nDate of birth ${DOB}\nphone ${phone}");
   print("the fourth digit of my number is ${phone[3]} \nI have ${Name.length} letter in my name");
